6fae16b58bf03b904158ac8bb62e9681771e04b0
tranhuuvinh1109@gmail.com <tranhuuvinh1109@gmail.com>
2025-02-23 10:33:11 +0700

    0.2-main: add cv

---
+++ src/components/DrawerCVContent/index.tsx
@@ -0,0 +1,16 @@
+import { Drawer } from "antd";
+import CV from "../../assets/pdf/CV_TRAN_HUU_VINH_DEVELOPER.pdf";
+type DrawerCVContentProps = {
+  onClose: () => void;
+  open: boolean;
+};
+
+const DrawerCVContent = ({ onClose, open }: DrawerCVContentProps) => {
+  return (
+    <Drawer title="Vinh Tran Huu" onClose={onClose} open={open} size="large">
+      <iframe src={CV} title="External Website" width="100%" height="100%" style={{ border: "none" }} />
+    </Drawer>
+  );
+};
+
+export default DrawerCVContent;
